0e9bdb31bb0832d7f4c8046f77d3629a75e4f7fb
Akim_Boyarin <48792298+Akim-boyarin@users.noreply.github.com>
2020-02-07 12:31:55 +0300

    12:31

--- pythagoreanTriples.js
+++ pythagoreanTriples.js
@@ -1,9 +1,10 @@
-var triples = [];
+let triples = [];
+let range = 300;
 
-for (var x = 1; x <= 300; x++) {
-    for (var y = 1; y <= 300; y++) {
-        var triple = {};
-        var z = Math.hypot(x, y);
+for (let x = 1; x <= range; x++) {
+    for (let y = 1; y <= range; y++) {
+        let triple = {};
+        let z = Math.hypot(x, y);
         if (Math.floor(z) === z) {
             triple.triple = [x, y, z];
             triple.pair = [x, y];
@@ -12,14 +13,14 @@ for (var x = 1; x <= 300; x++) {
     }
 }
 
-for (var i = 0; i < triples.length; i++) {
-    for (var j = 0; j < triples.length; j++) {
+for (let i = 0; i < triples.length; i++) {
+    for (let j = 0; j < triples.length; j++) {
         if (triples[i].pair.slice().join("") === triples[j].pair.slice().reverse().join("")) {
             triples.splice(j, 1);
         }
     }    
 }
 
-for (var i = 0; i < triples.length; i++) {
+for (let i = 0; i < triples.length; i++) {
     console.log(triples[i].triple.join("; "));
 }
