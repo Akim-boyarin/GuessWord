fe89728a98f059005c9247f39dbd29250f6d5441
Akim_Boyarin <48792298+Akim-boyarin@users.noreply.github.com>
2020-08-20 16:12:51 +0300

    the function is supplemented by transformations through the reduction formulas, and calculations not only through the sine series, but also the cosine series

--- calculateSin.js
+++ calculateSin.js
@@ -1,24 +1,68 @@
-// вычисление синуса угла в градусах через формулу ряда
+// вычисление синуса угла в градусах
 
-function calculateSin (arg) { // аргумент в градусах
-	if (arg / 360 >= 1) {
-		arg -= Math.floor(arg / 360) * 360;
+function calculateSin(angle) {
+	// преобразовать в аналогичное значение угла, меньшее 360
+	if ((angle / 360) >= 1) {
+		angle -= Math.floor(angle / 360) * 360;
 	}
-	let argRad = arg * Math.PI / 180; // аргумент в радианах
+
+	// преобразование вычисляемого угла по формулам приведения и определение способа вычисления
+	let calculationDataAndParams = determineTheCalculationMethod(angle);
+
+	// перевод вычисляемого значения угла в радианы
+	calculationDataAndParams.angleValue = calculationDataAndParams.angleValue * Math.PI / 180;
+
 	let sin = 0;
 	let maxIterationValue = 120;
 	for (let i = 0; i <= maxIterationValue; i++) {
-		sin += (Math.pow(-1, i) * Math.pow(argRad, (2 * i + 1))) / calculateFactorial(2 * i + 1);
+		if (calculationDataAndParams.isSin) {
+			// вычисление по ряду синуса
+			sin += ((-1) ** i ) * calculationDataAndParams.angleValue ** (2 * i + 1) / getFactorial(2 * i + 1);
+		} else {
+			// вычисление по ряду косинуса
+			sin += ((-1) ** i) * calculationDataAndParams.angleValue ** (2 * i) / getFactorial(2 * i);
+		}
 	}
+
+	if (!calculationDataAndParams.isPositiveSign) sin *= -1;
+
 	return sin;
 }
 
-function calculateFactorial(arg) {
+function determineTheCalculationMethod(angle) {
+	let data = {};
+
+	if (angle >= 270) {
+		data.angleValue = angle - 270;
+		data.isSin = false;
+		data.isPositiveSign = false;
+	} else if (angle >= 180) {
+		data.angleValue = angle - 180;
+		data.isSin = true;
+		data.isPositiveSign = false;
+	} else if (angle >= 90) {
+		data.angleValue = angle - 90;
+		data.isSin = false;
+		data.isPositiveSign = true;
+	} else {
+		data.angleValue = angle;
+		data.isSin = true;
+		data.isPositiveSign = true;
+	}
+
+	return data;
+}
+
+
+function getFactorial(arg) {
 	let result = 1;
+
 	for (let i = 1; i <= arg; i++) {
 		result *= i;
 	}
+
 	return result;
 }
 
-console.log(calculateSin(1));
+
+console.log(calculateSin(30));
